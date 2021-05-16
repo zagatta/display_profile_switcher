2a704b7616e7db7e6f76123992aaa96e45d39fd8
Jonas Zagatta <zagatta@sonah.tech>
2021-05-16 18:36:02 +0200

    creates .local/share/gnome-shell/extensions/

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 rm -rf $HOME/.local/share/gnome-shell/extensions/display_profile_switcher@zagatta.tech
-cp -R display_profile_switcher@zagatta.tech $HOME/.local/share/gnome-shell/extensions/display_profile_switcher@zagatta.tech/
+mkdir $HOME/.local/share/gnome-shell/extensions/
+cp -R display_profile_switcher@zagatta.tech $HOME/.local/share/gnome-shell/extensions/
