60247823d42941308ddfd58448eedd4de84966d2
Jonas Zagatta <zagatta@sonah.tech>
2021-05-16 17:29:36 +0200

    fix installation

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 rm -rf $HOME/.local/share/gnome-shell/extensions/display_profile_switcher@zagatta.tech
-cp -R display_profile_switcher@zagatta.tech $HOME/.local/share/gnome-shell/extensions/
+cp -R display_profile_switcher@zagatta.tech $HOME/.local/share/gnome-shell/extensions/display_profile_switcher@zagatta.tech/
